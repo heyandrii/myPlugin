6cd72e9484d6940b71419dfcce638abb22366704
Andrii Klymenko <andrew.klimenko@genovawebart.com>
2017-10-26 16:13:08 +0300

    [create] remove comments;

--- division-bell/gulpfile.js
+++ division-bell/gulpfile.js
@@ -5,32 +5,32 @@ var gulp = require('gulp'),
     csso = require('gulp-csso'),
     sass = require('gulp-sass');
 
-gulp.task('sass', function () { 
-	gulp.src('./assets/styles/style.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(csso())
-		.pipe(gulp.dest('./public/css/'));
+gulp.task('sass', function () {
+    gulp.src('./assets/styles/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(csso())
+        .pipe(gulp.dest('./public/css/'));
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
     gulp.src([
-                './assets/javascripts/jquery-2.1.4.js',
-                './assets/javascripts/bootstrap.js',
-                './assets/javascripts/script.js'
-        ])
+        './assets/javascripts/jquery-2.1.4.js',
+        './assets/javascripts/bootstrap.js',
+        './assets/javascripts/script.js'
+    ])
         .pipe(concat('min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./public/js/'))
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
     gulp.src('.assets/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./public/images/'))
 });
 
 gulp.task('watch', function () {
-	gulp.watch('./assets/styles/**/*.scss', ['sass']);
-	gulp.watch('./assets/javascripts/**/*.js', ['js']);
-	gulp.watch('./assets/images/**/*', ['images']);
+    gulp.watch('./assets/styles/**/*.scss', ['sass']);
+    gulp.watch('./assets/javascripts/**/*.js', ['js']);
+    gulp.watch('./assets/images/**/*', ['images']);
 });
